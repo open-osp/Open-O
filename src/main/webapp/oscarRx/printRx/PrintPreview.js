93a6623050c2f92ee201a5c92190aac18eab3dee
Chitrank Davé <chitrankd91@gmail.com>
2025-05-20 12:48:58 -0400

    Add PrintPreview.js and PrintPreview.jsp for new Rx preview

---
+++ src/main/webapp/oscarRx/printRx/PrintPreview.js
@@ -0,0 +1,257 @@
+/**
+ * Copyright (c) 2005-2012. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * <p>
+ * This software was written for
+ * Centre for Research on Inner City Health, St. Michael's Hospital,
+ * Toronto, Ontario, Canada
+ */
+
+function resetStash(ctx) {
+    const url = ctx + "/oscarRx/deleteRx.do?parameterValue=clearStash";
+    const data = "";
+    new Ajax.Request(url, {
+        method: 'post', parameters: data, onSuccess: function (transport) {
+            updateCurrentInteractions();
+        }
+    });
+    parent.document.getElementById('rxText').innerHTML = "";//make pending prescriptions disappear.
+    parent.document.getElementById('searchString').focus();
+}
+
+function resetReRxDrugList(ctx) {
+    const url = ctx + "/oscarRx/deleteRx.do?parameterValue=clearReRxDrugList";
+    const data = "";
+    new Ajax.Request(url, {
+        method: 'post', parameters: data, onSuccess: function (transport) {
+        }
+    });
+}
+
+function setComment() {
+    frames['preview'].document.getElementById('additNotes').innerHTML = '<%=Encode.forJavaScript(comment.replaceAll("\n", "<br>"))%>';
+    frames['preview'].document.getElementsByName('additNotes')[0].value = frames['preview'].document.getElementById('additNotes').innerHTML;
+}
+
+function setDefaultAddr() {
+    const url = "setDefaultAddr.jsp";
+    const ran_number = Math.round(Math.random() * 1000000);
+    const addr = encodeURIComponent(document.getElementById('addressSel').value);
+    const params = "addr=" + addr + "&rand=" + ran_number;
+    new Ajax.Request(url, {method: 'post', parameters: params});
+}
+
+function addNotes(scriptId) {
+    const url = "AddRxComment.jsp";
+    const ran_number = Math.round(Math.random() * 1000000);
+    const comment = encodeURIComponent(document.getElementById('additionalNotes').value);
+    const params = "scriptNo=" + scriptId + "&comment=" + comment + "&rand=" + ran_number;  //]
+    new Ajax.Request(url, {method: 'post', parameters: params});
+    frames['preview'].document.getElementById('additNotes').innerHTML = document.getElementById('additionalNotes').value.replace(/\n/g, "<br>");
+    frames['preview'].document.getElementsByName('additNotes')[0].value = document.getElementById('additionalNotes').value.replace(/\n/g, "\r\n");
+}
+
+function printIframe() {
+    const browserName = navigator.appName;
+    if (browserName === "Microsoft Internet Explorer") {
+        alert("Use of Microsoft Internet Explorer is not permitted")
+    } else {
+        if ('function' === typeof window.onbeforeunload) {
+            window.onbeforeunload = null;
+        }
+
+        preview.focus();
+        preview.print();
+
+        self.onfocus = function () {
+            self.setTimeout(function () {
+                self.parent.close();
+            }, 1000);
+        };
+        self.focus();
+    }
+}
+
+function writeToEncounter(ctx, print, text, prefPharmacy, providerNo, demographicNo, curProviderNo, curDate, userName) {
+    try {
+        const url = ctx + "/oscarRx/WriteToEncounter.do";
+        new Ajax.Request(url, {
+            method: 'post',
+            parameters: "prefPharmacy=" + encodeURIComponent(prefPharmacy) + "&additionalNotes=" + "&body=" + encodeURIComponent(text),
+            onSuccess: function (ret) {
+                //console.log("success")
+                if (print) {
+                    printIframe();
+                }
+                openEncounter(providerNo, demographicNo, curProviderNo, userName);
+            },
+            onError: function (e) {
+                alert("ERROR: could not paste to EMR" + e);
+                if (print) {
+                    printIframe();
+                }
+                openEncounter(providerNo, demographicNo, curProviderNo, userName);
+            }
+        });
+    } catch (e) {
+        alert("ERROR: could not paste to EMR" + e);
+    }
+}
+
+function openEncounter(providerNo, demographicNo, curProviderNo, userName) {
+    const windowProps = "height=710,width=1024,location=no,scrollbars=yes,menubars=no,toolbars=no,resizable=yes,screenX=50,screenY=50,top=20,left=20";
+    const currentDate = new Date().toISOString().substring(0, 10);
+    const url = "../oscarEncounter/IncomingEncounter.do?providerNo=" + providerNo + "&demographicNo=" + demographicNo + "&curProviderNo=" + curProviderNo + "&userName=" + userName + "&curDate=" + currentDate;
+
+    if (window.parent.opener && window.parent.opener.document.forms["caseManagementEntryForm"] !== undefined) {
+        // redirect if encounter window open
+        window.parent.opener.location = url;
+        return window.parent.opener;
+    }
+
+    return window.open(url, "encounter", windowProps);
+}
+
+function expandPreview(text, warning) {
+    parent.document.getElementById('lightwindow_container').style.width = "1140px";
+    // parent.document.getElementById('lightwindow_container').style.left="-600px"; // commented to resolve a UI alignment issue causing the print-rx window to shift off-screen to the left.
+    parent.document.getElementById('lightwindow_contents').style.width = "1120px";
+    document.getElementById('preview').style.width = "600px";
+    frames['preview'].document.getElementById('pharmInfo').innerHTML = text;
+    //frames['preview'].document.getElementById('removePharm').show();
+    $("selectedPharmacy").innerHTML = warning;
+
+}
+
+function reducePreview() {
+    parent.document.getElementById('lightwindow_container').style.width = "1000px";
+    parent.document.getElementById('lightwindow_contents').style.width = "980px";
+    document.getElementById('preview').style.width = "460px";
+    frames['preview'].document.getElementById('pharmInfo').innerHTML = "";
+    $("selectedPharmacy").innerHTML = "";
+}
+
+function toggleView(form) {
+    var dxCode = (form.hsfo_Hypertension.checked ? 1 : 0) + (form.hsfo_Diabetes.checked ? 2 : 0) + (form.hsfo_Dyslipidemia.checked ? 4 : 0);
+    // send dx code to HsfoPreview.jsp so that it will be displayed and persisted there
+    document.getElementById("hsfoPop").style.display = "none";
+    document.getElementById("bodyView").style.display = "block";
+    document.getElementById("preview").src = "HsfoPreview.jsp?dxCode=" + dxCode;
+}
+
+function clearPending(action) {
+    document.forms.RxClearPendingForm.action.value = action;
+    document.forms.RxClearPendingForm.submit();
+}
+
+function ShowDrugInfo(drug) {
+    window.open("drugInfo.do?GN=" + escape(drug), "_blank", "location=no, menubar=no, toolbar=no, scrollbars=yes, status=yes, resizable=yes");
+}
+
+function printPharmacy(ctx, id, warning) {
+    //ajax call to get all info about a pharmacy
+    //use json to write to html
+    if (!id) {
+        return;
+    }
+    const url = ctx + "/oscarRx/managePharmacy2.do?";
+    const data = "method=getPharmacyInfo&pharmacyId=" + id;
+    new Ajax.Request(url, {
+        method: 'get', parameters: data, onSuccess: function (transport) {
+            const json = transport.responseText.evalJSON();
+
+            if (json != null) {
+                let text = json.name + "<br>" + json.address + "<br>" + json.city + ", " + json.province + ", " + json.postalCode + "<br>Tel:" + json.phone1 + " " + json.phone2 + "<br>Fax:" + json.fax + "<br>Email:" + json.email + "<br>Note:" + json.notes;
+
+                text += '<br><br><a class="noprint" style="text-align:center;" onclick="parent.reducePreview();" href="javascript:void(0);">Remove Pharmacy Info</a>';
+                text += "<input type='hidden' name='pharmacyInfo' value=" + id + " />"
+                expandPreview(text, warning);
+            }
+        }
+    });
+
+}
+
+function refreshImage(imgURL, signatureImg) {
+    counter = counter + 1;
+    if (frames["preview"].document.getElementById("signature") != null) {
+        frames["preview"].document.getElementById("signature").src = imgURL + "&rand=" + counter;
+    }
+    frames['preview'].document.getElementById('imgFile').value = signatureImg;
+}
+
+function unloadMess() {
+    let mess = "Signature found, but fax has not been sent.";
+    if (isSignatureDirty) {
+        mess = "Signature changed, but not saved and fax not sent.";
+    }
+    return mess;
+}
+
+function setDigitalSignatureToRx(ctx, digitalSignatureId, scriptId) {
+    new Ajax.Request(ctx + '/oscarRx/saveDigitalSignature.do?method=saveDigitalSignature' + '&digitalSignatureId=' + digitalSignatureId + "&scriptId=" + scriptId, {
+        method: 'get'
+    });
+}
+
+function toggleFaxButtons(disabled) {
+    document.getElementById("faxButton").disabled = disabled;
+    document.getElementById("faxPasteButton").disabled = disabled;
+}
+
+function showFaxWarning() {
+    if (typeof hasFaxNumber !== 'undefined' && !hasFaxNumber) {
+        document.getElementById("faxWarningNote").style.display = "block";
+    }
+}
+
+function signatureHandler(e) {
+    isSignatureDirty = e.isDirty;
+    isSignatureSaved = e.isSave;
+    e.target.onbeforeunload = null;
+
+    if (window.hasFaxNumber !== undefined) {
+        let disabled = !hasFaxNumber || !e.isSave;
+        toggleFaxButtons(disabled);
+    }
+
+    if (e.isSave) {
+        if (window.hasFaxNumber && hasFaxNumber) {
+            e.target.onbeforeunload = unloadMess;
+        }
+
+        if (window.setDigitalSignatureToRx && window.contextPath && window.scriptNo) {
+            try {
+                let signId = new URLSearchParams(e.storedImageUrl.split('?')[1]).get('digitalSignatureId');
+                setDigitalSignatureToRx(contextPath, signId, scriptNo);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        if (window.refreshImage && window.imageUrl && window.tempPath) {
+            refreshImage(imageUrl, tempPath);
+        }
+    }
+}
+
+function closeRxPreviewBootstrapModal() {
+    const modal = document.getElementById('rxPreviewBootstrapModal');
+    let bsModal = bootstrap.Modal.getInstance(modal);
+    if (bsModal) {
+        bsModal.hide();
+    }
+}
