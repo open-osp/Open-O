0fed2b1a8d3b63712f97fbb13c80e90a6fd70a40
LiamStanziani <LiamStanziani1@outlook.com>
2026-01-26 16:16:21 -0500

    fix: added IF NOT EXISTS in index update

--- database/mysql/updates/update-2026-01-26-tickler-indexes.sql
+++ database/mysql/updates/update-2026-01-26-tickler-indexes.sql
@@ -5,7 +5,7 @@
 CREATE INDEX IF NOT EXISTS `idx_tickler_link_tickler_no` ON `tickler_link` (`tickler_no`);
 
 -- Index for composite key
-CREATE INDEX `idx_tickler_status_service_date` ON `tickler` (`status`, `service_date`);
+CREATE INDEX IF NOT EXISTS `idx_tickler_status_service_date` ON `tickler` (`status`, `service_date`);
 
 -- Index for "assigned to" filter queries
 CREATE INDEX IF NOT EXISTS `idx_tickler_task_assigned_to` ON `tickler` (`task_assigned_to`);
