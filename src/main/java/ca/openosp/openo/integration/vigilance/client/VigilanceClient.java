56023bb591d5eaf88cee5bd32828ff3c5c65dcf0
Chitrank Davé <chitrankd91@gmail.com>
2026-05-06 20:38:24 -0400

    feat(vigilance): add OAuth2 config & HTTP client
    - Implement VigilanceOAuth2ClientProvider to configure the specialized vigilanceWebClient bean.
    - Add VigilanceClient as a low-level wrapper for communicating with Vigilance API endpoints.
    - Enable OAuth2 authentication specifically tailored for Vigilance authorization server requirements, including the mandatory user_id.
    - Provide generic methods for POST requests and health status checks via the Status API.

---
+++ src/main/java/ca/openosp/openo/integration/vigilance/client/VigilanceClient.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2005-2012. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * <p>
+ * This software was written for
+ * Centre for Research on Inner City Health, St. Michael's Hospital,
+ * Toronto, Ontario, Canada
+ */
+
+package ca.openosp.openo.integration.vigilance.client;
+
+import ca.openosp.openo.integration.vigilance.exception.VigilanceIntegrationException;
+import ca.openosp.openo.integration.vigilance.model.VigilanceRequest;
+import ca.openosp.openo.integration.vigilance.model.VigilanceResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+
+/**
+ * Client for interacting with the Vigilance integration services.
+ * Provides methods to send requests and retrieve status information from the Vigilance API.
+ */
+@Component
+public class VigilanceClient {
+
+    @Value("${vigilance.base.url:}")
+    private String baseUrl;
+
+    @Value("${vigilance.status.base.url:}")
+    private String statusBaseUrl;
+
+    @Value("${vigilance.status.endpoint:}")
+    private String statusEndpoint;
+
+    private final WebClient vigilanceWebClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Constructs a new VigilanceClient.
+     *
+     * @param vigilanceWebClient the OAuth2-configured WebClient for Vigilance API calls
+     */
+    public VigilanceClient(
+            @Qualifier("vigilanceWebClient") WebClient vigilanceWebClient) {
+        this.vigilanceWebClient = vigilanceWebClient;
+    }
+
+    /**
+     * Sends a POST request to the specified service endpoint with a request body serialized as JSON.
+     * The request is sent as application/x-www-form-urlencoded with the JSON body in the 'intrant' field.
+     *
+     * @param <T> the type of the response, must extend VigilanceResponse
+     * @param serviceEndPoint the endpoint path to call
+     * @param requestBody the object to be serialized as JSON and sent
+     * @param responseType the class of the expected response
+     * @return the deserialized response object
+     * @throws VigilanceIntegrationException if serialization fails or the API returns an error
+     */
+    public <T extends VigilanceResponse> T postForObject(String serviceEndPoint, VigilanceRequest requestBody, Class<T> responseType) {
+        try {
+            String jsonBody = objectMapper.writeValueAsString(requestBody);
+            MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+            formData.add("intrant", jsonBody);
+
+            return this.vigilanceWebClient.post()
+                    .uri(UriComponentsBuilder.fromHttpUrl(baseUrl)
+                            .path(serviceEndPoint)
+                            .build()
+                            .toUri()
+                    )
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .bodyValue(formData)
+                    .retrieve()
+                    .onStatus(HttpStatus::isError, response ->
+                        response.bodyToMono(String.class)
+                                .flatMap(body -> reactor.core.publisher.Mono.error(
+                                        new VigilanceIntegrationException("Vigilance API error: " + response.statusCode() + " - " + body)))
+                    )
+                    .bodyToMono(responseType)
+                    .block();
+        } catch (IOException e) {
+            throw new VigilanceIntegrationException("Failed to serialize request body", e);
+        } catch (Exception e) {
+            if (e instanceof VigilanceIntegrationException) throw e;
+            throw new VigilanceIntegrationException("Unexpected error during Vigilance API call", e);
+        }
+    }
+
+    /**
+     * Sends a simple POST request to the specified service endpoint.
+     *
+     * @param serviceEndPoint the endpoint path to call
+     * @return the response body as a string
+     */
+    public String post(String serviceEndPoint) {
+        return this.vigilanceWebClient.post()
+                .uri(UriComponentsBuilder.fromHttpUrl(baseUrl).path(serviceEndPoint).build().toUri())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    /**
+     * Makes a GET request to the status API endpoint.
+     * Status API uses a different host than other Vigilance services.
+     */
+    public String getStatus() {
+        return this.vigilanceWebClient.get()
+                .uri(UriComponentsBuilder.fromHttpUrl(statusBaseUrl)
+                        .path(statusEndpoint)
+                        .build()
+                        .toUri()
+                )
+                .retrieve()
+                .onStatus(HttpStatus::isError, response ->
+                    response.bodyToMono(String.class)
+                            .flatMap(body -> reactor.core.publisher.Mono.error(
+                                    new VigilanceIntegrationException("Vigilance Status API error: " + response.statusCode() + " - " + body)
+                            ))
+                )
+                .bodyToMono(String.class)
+                .block();
+    }
+}
---
+++ src/main/java/ca/openosp/openo/integration/vigilance/config/VigilanceOAuth2ClientProvider.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2005-2012. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * <p>
+ * This software was written for
+ * Centre for Research on Inner City Health, St. Michael's Hospital,
+ * Toronto, Ontario, Canada
+ */
+
+package ca.openosp.openo.integration.vigilance.config;
+
+import ca.openosp.openo.webserv.oauth2.OpenOOAuth2ClientProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * OAuth2 client provider for the Vigilance integration.
+ * Extends {@link OpenOOAuth2ClientProvider} to provide custom token parameters
+ * and a specialized WebClient bean for Vigilance API calls.
+ */
+@Configuration
+public class VigilanceOAuth2ClientProvider extends OpenOOAuth2ClientProvider {
+
+    private final String userId;
+
+    /**
+     * Constructs the provider with core OAuth2 credentials.
+     *
+     * @param clientId     the OAuth2 client ID
+     * @param clientSecret the OAuth2 client secret
+     * @param tokenUri     the URI of the authorization server's token endpoint
+     * @param userId       the user_id required by the Vigilance authorization server
+     */
+    public VigilanceOAuth2ClientProvider(
+            @Value("${vigilance.oauth2.client.id:}") String clientId,
+            @Value("${vigilance.oauth2.client.secret:}") String clientSecret,
+            @Value("${vigilance.oauth2.client.token_uri:}") String tokenUri,
+            @Value("${vigilance.oauth2.client.user_id:}") String userId) {
+        super(clientId, clientSecret, tokenUri);
+        this.userId = userId;
+    }
+
+    /**
+     * Adds custom parameters to the OAuth2 token request.
+     * Specifically adds the 'user_id' required by the Vigilance authorization server.
+     *
+     * @return A MultiValueMap containing the custom token parameters
+     */
+    @Override
+    protected MultiValueMap<String, String> getCustomTokenParameters() {
+        MultiValueMap<String, String> params = super.getCustomTokenParameters();
+        params.add("user_id", this.userId);
+        return params;
+    }
+
+    /**
+     * Creates and configures the WebClient used for Vigilance API integrations.
+     *
+     * @return a configured {@link WebClient} instance
+     */
+    @Bean(name = "vigilanceWebClient")
+    public WebClient getVigilanceClient() {
+        return super.buildClient();
+    }
+}
